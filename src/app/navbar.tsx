9a41966419b772bf4665430dc442254f030a20a7
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-09-26 15:22:22 +0700

    update features login and register using Link

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,23 +1,50 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { Fragment } from "react";
 
 export default function Navbar() {
+  const pathName = usePathname();
+
   return (
     <Fragment>
       <nav className="flex bg-gray-800 py-2 px-5">
         <h1 className="text-white">Navbar</h1>
         <ul className="flex ml-5">
           <Link href="/">
-            <li className="mr-3 text-blue-300 cursor-pointer ">Home</li>
+            <li
+              className={`mr-3 ${
+                pathName === "/" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Home
+            </li>
           </Link>
           <Link href="/about">
-            <li className="mr-3 text-blue-300 cursor-pointer ">About</li>
+            <li
+              className={`mr-3 ${
+                pathName === "/about" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              About
+            </li>
           </Link>
           <Link href="/about/profile">
-            <li className="mr-3 text-blue-300 cursor-pointer ">Profile</li>
+            <li
+              className={`mr-3 ${
+                pathName === "/about/profile" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Profile
+            </li>
           </Link>
           <Link href="/contact">
-            <li className="mr-3 text-blue-300 cursor-pointer ">Contact</li>
+            <li
+              className={`mr-3 ${
+                pathName === "/contact" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Contact
+            </li>
           </Link>
         </ul>
       </nav>
