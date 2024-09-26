514b2559ff62e656daee7e1fa990d8a13174ece2
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-09-26 09:42:39 +0700

    first commit

---
+++ src/app/navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React, { Fragment } from "react";
+
+export default function Navbar() {
+  return (
+    <Fragment>
+      <nav className="flex bg-gray-800 py-2 px-5">
+        <h1 className="text-white">Navbar</h1>
+        <ul className="flex ml-5">
+          <Link href="/">
+            <li className="mr-3 text-blue-300 cursor-pointer ">Home</li>
+          </Link>
+          <Link href="/about">
+            <li className="mr-3 text-blue-300 cursor-pointer ">About</li>
+          </Link>
+          <Link href="/about/profile-page">
+            <li className="mr-3 text-blue-300 cursor-pointer ">Profile</li>
+          </Link>
+        </ul>
+      </nav>
+    </Fragment>
+  );
+}
