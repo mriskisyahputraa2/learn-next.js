880dfa19cabae8f85388424883c42fc53cedda6a
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-09-29 14:26:53 +0700

    update middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default function Middleware(request: NextRequest) {
-  const isLogin = false;
+  const isLogin = true;
 
   if (!isLogin) {
     return NextResponse.redirect(new URL("/login", request.url));
