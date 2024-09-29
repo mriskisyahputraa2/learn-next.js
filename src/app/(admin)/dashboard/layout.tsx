7454c2a59d90c6345313389932d4756abeae58c9
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-09-29 14:28:31 +0700

    add Parallel Routes ractices in products, books and anlytics

---
+++ src/app/(admin)/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+export default function Layout({
+  children,
+  products,
+  analytics,
+  books,
+  payments,
+}: {
+  children: React.ReactNode;
+  products: React.ReactNode;
+  analytics: React.ReactNode;
+  books: React.ReactNode;
+  payments: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="p-5">
+        <div>{children}</div>
+        <div className="mt-5 flex justify-center items-center">
+          {products}
+          {analytics}
+        </div>
+        <div>{books}</div>
+        <div>{payments}</div>
+      </div>
+    </>
+  );
+}
