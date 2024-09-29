0b44ae2a5afeea3788a6e56cebdefa95416a3d5d
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-09-29 14:30:54 +0700

    add default.js feature in payment if web payment does not exist (404)

---
+++ src/app/(admin)/dashboard/@payments/page.tsx
@@ -0,0 +1,9 @@
+export default function PaymentsPage() {
+  return (
+    <>
+      <div className="w-3/6 h-96 bg-gray-300 rounded-[12px] flex justify-center items-center mt-5 mx-auto">
+        <h1>Payments</h1>
+      </div>
+    </>
+  );
+}
--- src/app/(admin)/dashboard/@products/page.tsx
+++ src/app/(admin)/dashboard/@products/page.tsx
@@ -2,35 +2,36 @@
 
 import { useState } from "react";
 
-export default function adminBookPage() {
+export default function adminProductPage() {
   const [status, setStatus] = useState("");
+
   const revalidate = async () => {
     const res = await fetch(
-      "http://localhost:3000/api/revalidate?tag=books&secret=@Mriski123",
+      "http://localhost:3000/api/revalidate?tag=products&secret=@Mriski123",
       {
         method: "POST",
       }
     );
 
     if (!res.ok) {
-      setStatus("Revalidation Failed");
+      setStatus("Revalidate Failed");
     } else {
       const response = await res.json();
       if (response.revalidate) {
-        setStatus("Revalidation Success");
+        setStatus("Revalidation successfully");
       }
     }
   };
 
   return (
     <>
-      <div>
+      <div className="w-3/6 h-96 bg-gray-300 rounded-[12px] flex justify-center items-center mr-5">
         <h1>{status}</h1>
         <button
           className="bg-cyan-800 p-3 text-white rounded-md m-2"
           onClick={() => revalidate()}
         >
-          Revalidate Books
+          Revalidate Products
         </button>
       </div>
     </>
