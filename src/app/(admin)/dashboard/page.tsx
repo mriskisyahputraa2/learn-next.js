590f316e664fccb3a67545eab745d2149e5d95e2
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-10-03 21:52:45 +0700

    change dashboard views and profile updates

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,25 +1,6 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function DashboardPage() {
-  const { data: session, status }: { data: any; status: string } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    // ?. (optional chaining), untuk mengakses user.role secara aman
-    if (status === "unauthenticated") {
-      router.push("/login");
-    } else {
-      if (session !== undefined && session?.user.role !== "admin") {
-        router.push("/");
-      }
-    }
-  }, [status, router, session?.user.role, session]);
-  // console.log(session);
-  // console.log(status);
   return (
     <>
       <div className="w-full h-96 bg-gray-300 rounded-[12px] flex justify-center items-center">
