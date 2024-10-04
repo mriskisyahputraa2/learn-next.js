4dccb68acb6b67a469afae03bd518eb3d1438dfe
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-10-04 12:02:35 +0700

    update middleware

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -22,19 +22,22 @@ export default function withAuth(
         secret: process.env.NEXTAUTH_SECRET,
       });
 
+      // cek jika user belum login
       if (!token && !authPage.includes(pathname)) {
         const url = new URL("/login", req.url);
         url.searchParams.set("callbackUrl", encodeURI(req.url));
-        return NextResponse.redirect(url);
+        return NextResponse.redirect(url); // redirect user kehalaman login, untuk login dulu
       }
 
+      // validasi, user sudah login dan sudah terdapat token
       if (token) {
+        // cek apakah authPage masuk sesuai dengan url pathname yaitu authPage('/login', '/register')
         if (authPage.includes(pathname)) {
-          return NextResponse.redirect(new URL("/", req.url));
+          return NextResponse.redirect(new URL("/", req.url)); // redirect user ke halaman home
         }
-
+        // cek apakah role nya adalah admin, dan hanya admin yang boleh masuk kehalaman AdminPage
         if (token.role !== "admin" && onlyAdminPage.includes(pathname)) {
-          return NextResponse.redirect(new URL("/", req.url));
+          return NextResponse.redirect(new URL("/", req.url)); // kalo bukan admin, redirect user kehalaman home
         }
       }
     }
