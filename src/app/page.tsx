747b4dbca56af0f6c428ec17f4dcbb996f02a1ff
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-10-10 09:00:51 +0700

    add error handling on home page and dahboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
+  throw new Error("Something went wrong");
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Hello World
