6cabe366a22eda83df1533991f19674af431739c
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-10-11 20:40:17 +0700

    Update views paymets

--- src/app/(admin)/dashboard/@payments/page.tsx
+++ src/app/(admin)/dashboard/@payments/page.tsx
@@ -1,7 +1,7 @@
 export default function PaymentsPage() {
   return (
     <>
-      <div className="w-3/6 h-96 bg-gray-300 rounded-[12px] flex justify-center items-center mt-5 mx-auto">
+      <div className="w-full h-96 bg-gray-300 rounded-[12px] flex justify-center items-center mt-5 mx-auto">
         <h1>Payments</h1>
       </div>
     </>
